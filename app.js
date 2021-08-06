3eea62638b3101b2a2c10eb896b928bf6ba826d4
lucian13fx <moldovan.1999@mail.ru>
2021-08-07 01:57:33 +0300

    com

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ function closeNav() {
   document.getElementById("filter").style.display = "none";
   document.body.style.overflow = "unset";
   document.body.style.height = "auto";
+    document.getElementById("nav").style.top = "0";
+
 }
 function openNav() {
   document.getElementById("ul").style.left = "0%";
   document.getElementById("filter").style.display = "block";
   document.body.style.overflow = "hidden";
-    document.body.style.height = "100%";
+  document.getElementById("nav").style.top = "-40px";
 
 
 }
