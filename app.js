58c0c650616b85d9bea77bb7948806bc63180291
lucian13fx <moldovan.1999@mail.ru>
2021-08-08 00:27:31 +0300

    commit

--- app.js
+++ app.js
@@ -2,17 +2,14 @@ function closeNav() {
   document.getElementById("ul").style.left = "-100%";
   document.getElementById("filter").style.display = "none";
   document.body.style.overflow = "unset";
-//  document.body.style.height = "auto";
-    document.getElementById("nav").style.top = "0";
-
+  //  document.body.style.height = "auto";
+  document.getElementById("nav").style.top = "0";
 }
 function openNav() {
   document.getElementById("ul").style.left = "0%";
   document.getElementById("filter").style.display = "block";
   document.body.style.overflow = "hidden";
   document.getElementById("nav").style.top = "-40px";
-
-
 }
 
 function myFunction() {
@@ -22,7 +19,12 @@ function myFunction() {
 
 function closeMsg() {
   document.getElementById("this").style.display = "none";
-  document.getElementById('filter1').style.display = "none";
+  document.getElementById("filter1").style.display = "none";
   document.body.style.overflow = "unset";
   document.body.style.height = "auto";
+}
+
+function ght() {
+  document.getElementById("wlcmcont").style.marginLeft = "5vw";
+  document.getElementById("nav").style.marginTop = "0";
 }
