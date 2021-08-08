bb7659a7746b0e8522572930390f7888122e7455
lucian13fx <moldovan.1999@mail.ru>
2021-08-08 16:43:03 +0300

    comm

--- app.js
+++ app.js
@@ -30,4 +30,24 @@ function ght() {
   document.getElementById("scrl").style.bottom = "50px";
 }
 
+//Dropdown CSS page
 
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function drpDown() {
+  document.getElementById("toptop").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("side2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+}
